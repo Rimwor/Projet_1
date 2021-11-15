ffd39fee5015fab7e288a7cc74a59e2593021ee0
Beata Krzyżosiak <80414308+Rimwor@users.noreply.github.com>
2021-11-15 14:05:08 +0100

    Controller dd($task)

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +14,18 @@ class TaskController extends AbstractController
      */
     public function index(): Response
     {
+        // On va chercher avec Doctrine le Repository de nos Task
+        $repository = $this->getDoctrine()->getRepository(Task::class);
+
+        // Dans ce repository nous recuperons toutes les donnes 
+        $task = $repository->findAll();
+
+        // Affichage de donnes dans la vue de ma var_dumper
+        dd($task);
+
+        // var_dump($task);
+        // die;
+
         return $this->render('task/index.html.twig', [
             'controller_name' => 'TaskController',
         ]);
