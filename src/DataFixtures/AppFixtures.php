3a7449251d30648155c612c7cc24364054b0d4c9
Beata Krzyżosiak <80414308+Rimwor@users.noreply.github.com>
2021-11-16 09:59:51 +0100

    Adding tags to the table with tasks

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,8 +2,9 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Task;
 use Faker\Factory;
+use App\Entity\Tag;
+use App\Entity\Task;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -17,7 +18,28 @@ class AppFixtures extends Fixture
         // Creation d\un nouvel objet Faker
         $faker = Factory::create('fr_FR');
 
+        // Creation de nos 5 categories 
+        // c = categorie
+        for ($c =  0; $c < 5; $c++) {
+
+            // Creation d'un nouvel objet Tag
+            $tag = new Tag;
+
+            // On ajout un nom a notre categorie
+            $tag->setName($faker->colorName());
+
+            // On fait persister les donnes
+            $manager->persist($tag);
+        }
+
+        // On push les categories en BDD
+        $manager->flush();
+
+        // Recuperation des categories crees
+        $tags = $manager->getRepository(Tag::class)->findAll();
+
         // Creation entre 15 et 30 taches aleatoirement
+        // t = task
         for ($t = 0; $t < mt_rand(15, 30); $t++) {
 
             // Creation d'un nouvel objet Task
@@ -29,7 +51,8 @@ class AppFixtures extends Fixture
                 ->setCreatedAt(new \DateTime())
                 // Attention les dates sont 
                 // fonctions du parametrage du server
-                ->setDueAt($faker->dateTimeBetween('now', '1 month'));
+                ->setDueAt($faker->dateTimeBetween('now', '1 month'))
+                ->setTag($faker->randomElement($tags));
 
             // On fait persister les donnes
             $manager->persist($task);
