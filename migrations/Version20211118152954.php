03ec3bd6526cb0defec4b590c94b7c412dd1f9c5
Beata Krzyżosiak <80414308+Rimwor@users.noreply.github.com>
2021-11-18 16:36:45 +0100

    INTERNALIZATION corrections

---
+++ migrations/Version20211118152954.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211118152954 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE status (id INT AUTO_INCREMENT NOT NULL, label VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE task ADD status_id INT DEFAULT NULL, CHANGE tag_id tag_id INT NOT NULL');
+        $this->addSql('ALTER TABLE task ADD CONSTRAINT FK_527EDB25BAD26311 FOREIGN KEY (tag_id) REFERENCES tag (id)');
+        $this->addSql('ALTER TABLE task ADD CONSTRAINT FK_527EDB256BF700BD FOREIGN KEY (status_id) REFERENCES status (id)');
+        $this->addSql('CREATE INDEX IDX_527EDB25BAD26311 ON task (tag_id)');
+        $this->addSql('CREATE INDEX IDX_527EDB256BF700BD ON task (status_id)');
+        $this->addSql('ALTER TABLE user CHANGE email email VARCHAR(180) NOT NULL, CHANGE roles roles JSON NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON user (email)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE task DROP FOREIGN KEY FK_527EDB256BF700BD');
+        $this->addSql('DROP TABLE status');
+        $this->addSql('ALTER TABLE task DROP FOREIGN KEY FK_527EDB25BAD26311');
+        $this->addSql('DROP INDEX IDX_527EDB25BAD26311 ON task');
+        $this->addSql('DROP INDEX IDX_527EDB256BF700BD ON task');
+        $this->addSql('ALTER TABLE task DROP status_id, CHANGE tag_id tag_id INT DEFAULT NULL');
+        $this->addSql('DROP INDEX UNIQ_8D93D649E7927C74 ON user');
+        $this->addSql('ALTER TABLE user CHANGE email email VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE roles roles VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+    }
+}
